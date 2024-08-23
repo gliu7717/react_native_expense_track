b932bf4c9b446ac0062f8d7dee4695ccd4573529
rayliu7717 <rayliu2004@gmail.com>
2024-08-22 21:19:46 -0400

    working expense list item

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -9,31 +9,55 @@ const DUMMY_EXPENSES = [
     id: 'e1',
     description: 'A pair of shoes',
     amount: 59.99,
-    date: new Date('2021-12-19')
+    date: new Date('2023-12-19')
   },
   {
     id: 'e2',
     description: 'A pair of trousers',
     amount: 89.29,
-    date: new Date('2022-01-05')
+    date: new Date('2024-01-05')
   },
   {
     id: 'e3',
     description: 'Some bananas',
     amount: 5.99,
-    date: new Date('2021-12-01')
+    date: new Date('2023-12-01')
   },
   {
     id: 'e4',
     description: 'A book',
     amount: 14.99,
-    date: new Date('2022-02-19')
+    date: new Date('2024-02-19')
   },
   {
     id: 'e5',
     description: 'Another book',
     amount: 18.59,
-    date: new Date('2022-02-18')
+    date: new Date('2024-02-18')
+  },
+  {
+    id: 'e6',
+    description: 'A pair of trousers',
+    amount: 89.29,
+    date: new Date('2024-01-05')
+  },
+  {
+    id: 'e7',
+    description: 'Some bananas',
+    amount: 5.99,
+    date: new Date('2023-12-01')
+  },
+  {
+    id: 'e8',
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2024-02-19')
+  },
+  {
+    id: 'e9',
+    description: 'Another book',
+    amount: 18.59,
+    date: new Date('2024-02-18')
   }
 ];
 
@@ -51,7 +75,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700
   }
 });
